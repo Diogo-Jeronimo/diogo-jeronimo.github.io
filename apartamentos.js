6dfcdac344e4e23da425e42f5ff39022fcc94ab1
Diogo-Jeronimo <diogo.aires.jeronimo@protonmail.com>
2022-06-11 17:13:03 +0100

    tweak

--- apartamentos.js
+++ apartamentos.js
@@ -1,22 +1,20 @@
-const deepFreeze = object => {
-  const propNames = Object.getOwnPropertyNames(object);
-  for (const name of propNames) {
-    const value = object[name];
-    if (value && typeof value === "object") {
-      deepFreeze(value);
-    }
-  }
-  return Object.freeze(object);
-};
-const descriptions = deepFreeze({
+const isEN = document.URL.includes("/en/");
+const descriptions = isEN ? {
+  "T0": ".",
+  "T0+1": ".",
+  "T1": ".",
+  "T1s": ".",
+  "T1+1": ".",
+  "T2": "."
+} : {
   "T0": "Sala comum com duas camas, kitchenette e casa de banho completa, capacidade para 2 pessoas.",
   "T0+1": "Sala comum com dois sofás mais uma cama de casal, kittchenette e casa de banho completa, mais um quarto pequeno com uma cama single capacidade total para 3 pessoas.",
   "T1": "Sala com sofá cama para duas pessoas, um quarto, kitchenette e casa de banho completa. Máximo 4 pessoas.",
   "T1s": "T1 superior com um terraço grande que rodeia todo o apartamento e com sol todo o dia.",
   "T1+1": "Sala com sofá cama para duas pessoas, um quarto com cama de casal e mais outro quarto com duas camas individuais ambos com terraço, cozinha e casa de banho completa. Máximo 6 pessoas.",
   "T2": "Sala com sofá cama para duas pessoas, um quarto com cama de casal com terraço e janela e mais outro quarto interior sem janela nem terraço mas ventilado, kitcnenette e casa de banho completa. Máximo 5 pessoas."
-});
-const pricesTable = deepFreeze({
+};
+const pricesTable = {
   dates: [
     {start: "2022-01-03", end: "2022-03-31"}, {start: "2022-04-01", end: "2022-05-31"},
     {start: "2022-06-01", end: "2022-06-30"}, {start: "2022-07-01", end: "2022-07-15"},
@@ -34,15 +32,15 @@ const pricesTable = deepFreeze({
     "T1+1": [55, 60, 85, 105, 120, 140, 120, 105, 100, 85, 60, 55, 150],
     "T2": [60, 65, 95, 115, 130, 150, 130, 115, 110, 95, 65, 60, 160]
   }
-});
-const slidesTable = deepFreeze({
+};
+const slidesTable = {
   "T0": {id: 13037, slides: ["/pics/4-403/1181.med.jpg", "/pics/4-403/1182.med.jpg", "/pics/4-403/1183.med.jpg", "/pics/4-403/1187.med.jpg", "/pics/4-403/1196.med.jpg", "/pics/4-403/1200.med.jpg", "/pics/4-403/1333.med.jpg", "/pics/4-403/1338.med.jpg", "/pics/4-403/1342.med.jpg", "/pics/4-403/1344.med.jpg", "/pics/4-403/1345.med.jpg", "/pics/4-403/1348.med.jpg", "/pics/4-403/1352.med.jpg", "/pics/4-403/1357.med.jpg"]},
   "T0+1": {id: 49298, slides: ["/pics/6-102/1152.med.jpg", "/pics/6-102/1153.med.jpg", "/pics/6-102/1154.med.jpg", "/pics/6-102/1161.med.jpg", "/pics/6-102/1163.med.jpg", "/pics/6-102/1188.med.jpg", "/pics/6-102/1190.med.jpg", "/pics/6-102/1274.med.jpg", "/pics/6-102/1292.med.jpg", "/pics/6-102/1294.med.jpg", "/pics/6-102/1363.med.jpg", "/pics/6-102/1366.med.jpg", "/pics/6-102/1368.med.jpg", "/pics/6-102/1374.med.jpg"]},
   "T1": {id: 22535, slides: ["/pics/4-105/1093.med.jpg", "/pics/4-105/1094.med.jpg", "/pics/4-105/1096.med.jpg", "/pics/4-105/1097.med.jpg", "/pics/4-105/1102.med.jpg", "/pics/4-105/1103.med.jpg", "/pics/4-105/1104.med.jpg", "/pics/4-105/1105.med.jpg", "/pics/4-105/1106.med.jpg", "/pics/4-105/1107.med.jpg", "/pics/4-105/1109.med.jpg", "/pics/4-105/1110.med.jpg", "/pics/4-105/1112.med.jpg"]},
   "T1s": {id: 22517, slides: ["/pics/4-305/0821.med.jpg", "/pics/4-305/0826.med.jpg", "/pics/4-305/0827.med.jpg", "/pics/4-305/0828.med.jpg", "/pics/4-305/0829.med.jpg", "/pics/4-305/0834.med.jpg", "/pics/4-305/0835.med.jpg", "/pics/4-305/0838.med.jpg", "/pics/4-305/0842.med.jpg", "/pics/4-305/0920.med.jpg", "/pics/4-305/0936.med.jpg"]},
   "T1+1": {id: 55772, slides: ["/pics/4-201/0715.med.jpg", "/pics/4-201/0717.med.jpg", "/pics/4-201/0718.med.jpg", "/pics/4-201/0722.med.jpg", "/pics/4-201/0723.med.jpg", "/pics/4-201/0728.med.jpg", "/pics/4-201/0729.med.jpg", "/pics/4-201/0730.med.jpg", "/pics/4-201/0732.med.jpg", "/pics/4-201/0733.med.jpg", "/pics/4-201/0739.med.jpg", "/pics/4-201/0743.med.jpg", "/pics/4-201/0746.med.jpg", "/pics/4-201/0749.med.jpg", "/pics/4-201/0754.med.jpg", "/pics/4-201/0757.med.jpg", "/pics/4-201/1348.med.jpg"]},
   "T2": {id: 22536, slides: ["/pics/4-106/0669.med.jpg", "/pics/4-106/0675.med.jpg", "/pics/4-106/0678.med.jpg", "/pics/4-106/0682.med.jpg", "/pics/4-106/0683.med.jpg", "/pics/4-106/0686.med.jpg", "/pics/4-106/0687.med.jpg", "/pics/4-106/0688.med.jpg", "/pics/4-106/0690.med.jpg", "/pics/4-106/0693.med.jpg", "/pics/4-106/0695.med.jpg", "/pics/4-106/0697.med.jpg", "/pics/4-106/0699.med.jpg", "/pics/4-106/0702.med.jpg", "/pics/4-106/0703.med.jpg", "/pics/4-106/0704.med.jpg", "/pics/4-106/0705.med.jpg", "/pics/4-106/0708.med.jpg"]}
-});
+};
 
 const typeSelect = document.getElementById("type-select");
 const slideButtons = document.getElementById("slide-buttons");
@@ -64,7 +62,9 @@ const selectionChange = () => {
       buttons[i].hidden = true;
     }
   }
-  slideCaption.innerHTML = "Alojamento Local: " + slidesTable[selectedId].id;
+  if(!isEN) {
+    slideCaption.innerHTML = "Alojamento Local: " + slidesTable[selectedId].id;
+  }
   for(let i = 0; i < prices.length; i++) {
     pricesTableElement.rows[i + 1].cells[1].innerHTML = prices[i] + "€";
   }
@@ -110,7 +110,7 @@ const calcDateRangePrice = () => {
       }
       numberNights++;
     }
-    priceCalcResult.innerHTML = `Preço por ${numberNights} noite(s): ${totalPrice}€`;
+    priceCalcResult.innerHTML = isEN ? `Price for ${numberNights} night(s): ${totalPrice}€` : `Preço por ${numberNights} noite(s): ${totalPrice}€`;
   } else if(inDatePicker.value === "") {
     inDatePicker.value = outDatePicker.value;
   } else if(outDatePicker.value === "") {
